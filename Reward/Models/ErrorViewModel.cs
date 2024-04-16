45206dee3b2634c3d2102b30bf133b7ce03f22ad
Diksha <Diksha@isvrd.com>
2024-04-16 18:14:21 +0530

    Add project files.

---
+++ Reward/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Reward.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
