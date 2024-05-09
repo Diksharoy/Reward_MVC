6e2e8a1f8842e1ca73c924ac224a23b09431fc08
Diksha <Diksha@isvrd.com>
2024-05-09 18:52:30 +0530

    9may

--- Reward/Controllers/CategoryController.cs
+++ Reward/Controllers/CategoryController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using bulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Reward.Models;
 
 namespace Reward.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController( ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            //var ObjCategoryList = _db.Categories.ToList();             we can write it also
+            List<Category> ObjCategoryList = _db.Categories.ToList();
+            return View(ObjCategoryList);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create( Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
